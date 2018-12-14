e697740ec0616bc35c897a028e3a474b2a2fe350
Ian Wold <ilwold@hotmail.com>
2018-12-14 14:24:07 -0600

    Update to Metalsharp NuGet package

--- src/Build/Program.cs
+++ src/Build/Program.cs
@@ -10,44 +10,44 @@ namespace Build
     class Program
     {
         static void Main(string[] args) =>
-            new MetalsharpDirectory("..\\..\\..\\..\\Site", ".\\")
-                .UseFrontmatter()
-                .UseMarkdown()
-                .Use(dir =>
-                {
-                    dir.Meta("posts", new List<Post>());
+            new MetalsharpProject("..\\..\\..\\..\\Site", ".\\")
+            .UseFrontmatter()
+            .UseMarkdown()
+            .Use(dir =>
+            {
+                dir.Meta("posts", new List<Post>());
 
-                    foreach (var file in dir.OutputFiles
-                        .Where(file => file.Directory == ".\\Posts")
-                        .OrderByDescending(file => DateTime.TryParse(file.Metadata["date"] as string, out var date) ? ((date.Year * 10000) + (date.Month * 100) + date.Day) : 0))
-                    {
-                        file.Metadata.Add("template", ".\\Templates\\article.liquid");
-                        file.Metadata.Add("highlight", "blog");
-
-                        (dir.Metadata["posts"] as List<Post>).Add(new Post(file.Metadata["title"] as string, file.Metadata["date"] as string, file.Metadata["description"] as string, file.Metadata["author"] as string, file.Name));
-                    }
-                })
-                .Use(dir => dir.AddOutput(new MetalsharpFile("", ".\\blog.html")
+                foreach (MetalsharpFile file in dir.OutputFiles
+                    .Where(file => file.Directory == ".\\Posts")
+                    .OrderByDescending(file => DateTime.TryParse(file.Metadata["date"] as string, out DateTime date) ? ((date.Year * 10000) + (date.Month * 100) + date.Day) : 0))
                 {
-                    Metadata = new Dictionary<string, object>()
-                    {
-                        ["posts"] = dir.Metadata["posts"],
-                        ["highlight"] = "blog",
-                        ["title"] = "Blog",
-                        ["template"] = ".\\Templates\\blog.liquid"
-                    }
-                }))
-                .Use(LeveledFiles)
-                .AddInput("..\\..\\..\\..\\Templates", ".\\Templates")
-                .UseFluidTemplate(".\\Templates\\layout.liquid")
-                .AddOutput("..\\..\\..\\..\\Static", ".\\")
-                .Build(new BuildOptions
-                {
-                    OutputDirectory = "built",
-                    ClearOutputDirectory = false
-                });
+                    file.Metadata.Add("template", ".\\Templates\\article.liquid");
+                    file.Metadata.Add("highlight", "blog");
 
-        static void LeveledFiles(MetalsharpDirectory directory)
+                    (dir.Metadata["posts"] as List<Post>).Add(new Post(file.Metadata["title"] as string, file.Metadata["date"] as string, file.Metadata["description"] as string, file.Metadata["author"] as string, file.Name));
+                }
+            })
+            .Use(dir => dir.AddOutput(new MetalsharpFile("", ".\\blog.html")
+            {
+                Metadata = new Dictionary<string, object>()
+                {
+                    ["posts"] = dir.Metadata["posts"],
+                    ["highlight"] = "blog",
+                    ["title"] = "Blog",
+                    ["template"] = ".\\Templates\\blog.liquid"
+                }
+            }))
+            .Use(LeveledFiles)
+            .AddInput("..\\..\\..\\..\\Templates", ".\\Templates")
+            .UseFluidTemplate(".\\Templates\\layout.liquid")
+            .AddOutput("..\\..\\..\\..\\Static", ".\\")
+            .Build(new BuildOptions
+            {
+                OutputDirectory = "built",
+                ClearOutputDirectory = false
+            });
+
+        static void LeveledFiles(MetalsharpProject directory)
         {
             foreach (var file in directory.InputFiles.Concat(directory.OutputFiles))
             {
--- src/Site/projects.md
+++ src/Site/projects.md
@@ -7,6 +7,18 @@
 
 # GitHub Projects
 
+[**Metalsharp**](https://github.com/ianwold/Metalsharp)
+
+A tiny and extendable C# library for generating static sites, inspired by the [Metalsmith](https://metalsmith.io/) library for JavsScript.
+
+[**Monokai Gray**](https://github.com/IanWold/MonokaiGray) is also on [Package Control](https://packagecontrol.io/packages/Monokai%20Gray)
+
+A Sublime Text 3 color scheme, the same as Monokai dark, but has true gray backgrounds. Useful with themes where the yellowey background of Monokai feels out of place. Pending interest, a Visual Studio version may be forthcoming...
+
+[**Rusty Calculator**](https://github.com/IanWold/RustyCalculator)
+
+Rusty Calculator is a small desktop calculator (for Windows) that can sit nicely on your taskbar and be called when needed. It's very nice.
+
 ## Language/Parsing
 
 [**SpracheJSON**](https://github.com/IanWold/SpracheJSON) is also on [NuGet](https://www.nuget.org/packages/SpracheJSON)
@@ -21,30 +33,16 @@ SpracheDown is a MarkDown parser written in Sprache for C#. A while ago at the I
 
 For my final project for my Lambda Calculus course, I designed a very hacky typed LC interpreter! Wooo.
 
-## Console Gaming
+## CLI Gaming
 
 [**Outrage Engine**](https://github.com/IanWold/OutrageEngine)
 
-A game engine for ASCII games in the Windows Console, because that's the best platform for cutting edge game development.
+A game engine for ASCII games in the Windows Command Prompt, because that's the best platform for cutting edge game development.
 
 [**Console2048**](https://github.com/IanWold/Console2048)
 
-This is a version of the popular 2048 game in the Windows console, because no other platform provides such clarity and authenticity for such a game.
+This is a version of the popular 2048 game in the Windows command prompt, because no other platform provides such clarity and authenticity for such a game.
 
 [**ConsoleMinesweeper**](https://github.com/IanWold/ConsoleMinesweeper)
 
-Minesweeper in the Windows console. See *Console2048*.
-
-## Miscellaneous
-
-[**Metalsharp**](https://github.com/ianwold/Metalsharp)
-
-A tiny and extendable C# library for generating static sites, inspired by the [Metalsmith](https://metalsmith.io/) library for JavsScript.
-
-[**Monokai Gray**](https://github.com/IanWold/MonokaiGray) is also on [Package Control](https://packagecontrol.io/packages/Monokai%20Gray)
-
-A Sublime Text 3 color scheme, the same as Monokai dark, but has true gray backgrounds. Useful with themes where the yellowey background of Monokai feels out of place. Pending interest, a Visual Studio version may be forthcoming...
-
-[**Rusty Calculator**](https://github.com/IanWold/RustyCalculator)
-
-Rusty Calculator is a small desktop calculator (for Windows) that can sit nicely on your taskbar and be called when needed. It's very nice.
+Minesweeper in the Windows command prompt. See *Console2048*.
